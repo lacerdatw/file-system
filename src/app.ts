1ee2e35f8626f9a465d4f6b5a88e22136edd4d47
lacerdatw <eduardo.lacerda@thoughtworks.com>
2026-06-18 17:44:57 -0300

    feat(cors): add cors bypass to server security

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import cors from "cors";
 import { authRouter } from "./modules/auth/auth.router";
 import { filesRouter } from "./modules/files/files.router";
 import { errorMiddleware } from "./middlewares/error.middleware";
 import { loggerMiddleware } from "./middlewares/logger.middleware";
 
 export const app = express();
-
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],}));
 app.use(express.json());
 app.use(loggerMiddleware);
 
